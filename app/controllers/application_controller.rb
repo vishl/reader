8bcc2105e045b4bb5750989896490b6f407c91c4
Vishal Parikh <vparikh1@gmail.com>
2012-02-06 00:12:41 -0800

    redirect to proper domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_filter :check_uri
   protect_from_forgery
+
+  def check_uri
+    if(Rails.env.production? && request.host != GlobalSettings.app_domain)
+      redirect_to request.protocol + GlobalSettings.app_domain + request.request_uri 
+    end
+  end
 end
