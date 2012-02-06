399a75f0fb49ab71e6c0943f5af4a2b89b0927b6
Vishal Parikh <vparikh1@gmail.com>
2012-02-05 17:00:05 -0800

    added youtube embedding

--- app/helpers/forums_helper.rb
+++ app/helpers/forums_helper.rb
@@ -5,7 +5,6 @@ module ForumsHelper
   end
 
   def bookmarklet(sid)
-    #"javascript:void((function(){var%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','#{GlobalSettings.app_protocol}#{GlobalSettings.app_domain}/javascripts/bookmarklet.js?r='+Math.random()*99999999);document.body.appendChild(e)})());"
     "javascript:void((function(){var%20e=document.createElement('script');e.setAttribute('type','text/javascript');e.setAttribute('charset','UTF-8');e.setAttribute('src','#{GlobalSettings.app_protocol}#{GlobalSettings.app_domain}/bookmarklet?sid=#{sid}&r='+Math.random()*99999999);document.body.appendChild(e)})());"
   end
 end
