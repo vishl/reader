04b3ac628228008848249d33d38d6be8af9ce76a
Vishal Parikh <vparikh1@gmail.com>
2012-02-06 23:50:54 -0800

    order by comments

--- app/controllers/forums_controller.rb
+++ app/controllers/forums_controller.rb
@@ -16,8 +16,14 @@ class ForumsController < ApplicationController
   def show
     @forum = Forum.find_by_sid(params[:sid])
     @post = @forum.posts.build(params[:post])
+    @showcomments = params[:showcomments].present?
     if(@forum)
-      @posts = @forum.posts.order("updated_at DESC").limit(20).includes(:comments)
+      if(@showcomments)
+        #shows only posts with comments and joins on comments
+        @posts = @forum.posts.joins("JOIN comments ON comments.post_id=posts.id").order("comments.created_at DESC").group("posts.id").limit(20).includes(:comments)
+      else
+        @posts = @forum.posts.order("updated_at DESC").limit(20).includes(:comments)
+      end
       @latest_post_id = @forum.posts.latest.first.id if @forum.posts.count>0
       @latest_comment_id = @forum.comments.latest.first.id if @forum.comments.count>0
     else
