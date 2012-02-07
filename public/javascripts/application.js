d0ef02c5f11fddc134c0fc3b2016269a4fcc9d8d
Vishal Parikh <vparikh1@gmail.com>
2012-02-06 23:15:50 -0800

    added times

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,30 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+var ONE_WEEK = 1000*60*60*24*7; //milliseconds
+var Days = ["Sun","Mon","Tue","Wed","Thurs","Fri","Sat"];
+var Months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+$(function(){
+    $(".date-delta").each(function(){
+        var datestr = $(this).html();
+        var date= new Date(datestr); //not sure why Date(datestr) doesn't work
+        var today = new Date();
+        var newdate = ""
+        var h = date.getHours();
+        var m = date.getMinutes();
+        var ampm = h>=12?"PM":"AM"
+        if(h>12)
+          h-=12;
+        if(h==0)
+          h=12;
+
+        if((date.getFullYear() == today.getFullYear()) &&
+          (date.getMonth() == today.getMonth()) &&
+          (date.getDate() == today.getDate())){
+          newdate = h+":"+(m<10?"0":"")+m+" "+ampm;
+        }else if(today-date < ONE_WEEK){
+          newdate = Days[date.getDay()]+" "+h+" "+ampm
+        }else{
+          newdate = Months[date.getMonth()]+" "+date.getDate();
+        }
+        $(this).html(newdate)
+    })
+})
+
