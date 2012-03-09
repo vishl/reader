d9f0f8589b46480f488ce7813e9a02c85b32329a
Vishal Parikh <vparikh1@gmail.com>
2012-03-08 17:54:28 -0800

    need to subscribe to post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,26 +6,6 @@ class CommentsController < ApplicationController
     message = ""
     #TODO make sure user has permission to post in this forum
     @forum = Forum.find_by_sid(params[:forum_id])
-#    if(@forum)
-#      @post = @forum.posts.find_by_id(params[:post_id])
-#      if(@post)
-#        @comment = @post.comments.build(params.slice(:content))
-#        if(@comment.save)
-#          #success
-#          render :json=>{'comment'=>@comment}
-#        else
-#          logger.error("Comment posting error: #{@comment.errors}")
-#          render :json=>@comment.errors, :status=>400
-#        end
-#      else
-#        logger.error("Comment posting error: can't find post with id #{params[:post_id]}")
-#        render :json=>{"post"=>"invalid ID"}, :status=>400
-#      end
-#    else
-#      logger.error("Comment posting error: can't find forum with id #{params[:forum_id]}")
-#      render :json=>{"post"=>"invalid ID"}, :status=>400
-#    end
-
     if(!@forum)
       logger.error("Comment posting error: can't find forum with id #{params[:forum_id]}")
       render :json=>{"post"=>"invalid ID"}, :status=>400
@@ -35,14 +15,18 @@ class CommentsController < ApplicationController
         logger.error("Comment posting error: can't find post with id #{params[:post_id]}")
         render :json=>{"post"=>"invalid ID"}, :status=>400
       else
-        @comment = @post.comments.build(params.slice(:content))
-        @comment.user_id = current_user.id
-        if(!@comment.save)
-          logger.error("Comment posting error: #{@comment.errors}")
-          render :json=>@comment.errors, :status=>400
+        if(!@forum.permission(current_user,:comment))
+          render :json=>{"authorization"=>"Permission denied"}, :status=>401
         else
-          #success
-          render :json=>{'comment'=>@comment}
+          @comment = @post.comments.build(params.slice(:content))
+          @comment.user_id = current_user.id
+          if(!@comment.save)
+            logger.error("Comment posting error: #{@comment.errors}")
+            render :json=>@comment.errors, :status=>400
+          else
+            #success
+            render :json=>{'comment'=>@comment}
+          end
         end
       end
     end
