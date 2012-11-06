c1fdb284d3ba142120870bf5e758af387b8ea9cf
Vishal Parikh <vparikh1@gmail.com>
2012-11-05 23:15:35 -0800

    remove stale migrate file

--- db/migrate/20120728225742_add_token_index_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddTokenIndexToUsers < ActiveRecord::Migration
-  def self.up
-    add_index :users, :reset_token, :unique=>true
-  end
-
-  def self.down
-    remove_index :users, :reset_token
-  end
-end
